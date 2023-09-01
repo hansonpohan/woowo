81d668e18a67c5826912d32386268767da193ee6
pohan chen <a0931771160@gmail.com>
2023-09-01 16:47:33 +0800

    first commit

---
+++ Fairy_project/ViewModels/LoginViewModels.cs
@@ -0,0 +1,16 @@
+﻿using Fairy_project.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Fairy_project.ViewModels
+{
+    public class LoginViewModels
+    {
+        public Memberss member { get; set; }
+        public Permissionss permissions { get; set; }
+        public Manufacturess manufactures { get; set; }
+
+        ISession Session { get; set; }
+
+    }
+}
+
